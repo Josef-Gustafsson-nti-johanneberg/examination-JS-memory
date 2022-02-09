b7f57608ac7e90c3cdb972ab2ffa95001408b4a5
Josef-Gustafsson-nti-johanneberg <74712478+Josef-Gustafsson-nti-johanneberg@users.noreply.github.com>
2022-02-09 19:16:23 +0100

    Summa sumarum, jag stylade sidan aningens, la in en poster bild i min header, en bugg jag hade innan var att poängen inte syntes från början, jag löste det genom att köra den javascript strängen en gån till innan man har börjat spelet. Jag la även in så varje bil har ett eget id och ett id för matching, detta för att hålla kol så man inte klickar på samma bild flera gånger, jag löste även en randomizer  funktion som blandar om min array med kort i. Jag drog även nytta av ett attribut på kort objektet (matched) så man inte kan klicka och få poäng på matchade kort.

--- src/js/script.js
+++ src/js/script.js
@@ -1,144 +1,168 @@
 let data = [
 {
     id: 1,
-    img: 'img/char-1.png',
-    matched: false,
-    flipped: false
-},
-{
-    id: 1,
+    match_id: 1,
     img: 'img/char-1.png',
     matched: false,
     flipped: false
 },
 {
     id: 2,
-    img: 'img/char-2.png',
-    matched: false,
-    flipped: false
-},
-{
-    id: 2,
-    img: 'img/char-2.png',
+    match_id: 1,
+    img: 'img/char-1.png',
     matched: false,
     flipped: false
 },
 {
     id: 3,
-    img: 'img/char-3.png',
-    matched: false,
-    flipped: false
-},
-{
-    id: 3,
-    img: 'img/char-3.png',
+    match_id: 2,
+    img: 'img/char-2.png',
     matched: false,
     flipped: false
 },
 {
     id: 4,
-    img: 'img/char-4.png',
-    matched: false,
-    flipped: false
-},
-{
-    id: 4,
-    img: 'img/char-4.png',
+    match_id: 2,
+    img: 'img/char-2.png',
     matched: false,
     flipped: false
 },
 {
     id: 5,
-    img: 'img/char-5.png',
-    matched: false,
-    flipped: false
-},
-{
-    id: 5,
-    img: 'img/char-5.png',
+    match_id: 3,
+    img: 'img/char-3.png',
     matched: false,
     flipped: false
 },
 {
     id: 6,
-    img: 'img/char-6.png',
-    matched: false,
-    flipped: false
-},
-{
-    id: 6,
-    img: 'img/char-6.png',
+    match_id: 3,
+    img: 'img/char-3.png',
     matched: false,
     flipped: false
 },
 {
     id: 7,
-    img: 'img/char-7.png',
-    matched: false,
-    flipped: false
-},
-{
-    id: 7,
-    img: 'img/char-7.png',
+    match_id: 4,
+    img: 'img/char-4.png',
     matched: false,
     flipped: false
 },
 {
     id: 8,
-    img: 'img/char-8.png',
-    matched: false,
-    flipped: false
-},
-{
-    id: 8,
-    img: 'img/char-8.png',
+    match_id: 4,
+    img: 'img/char-4.png',
     matched: false,
     flipped: false
 },
 {
     id: 9,
-    img: 'img/char-9.png',
-    matched: false,
-    flipped: false
-},
-{
-    id: 9,
-    img: 'img/char-9.png',
+    match_id: 5,
+    img: 'img/char-5.png',
     matched: false,
     flipped: false
 },
 {
     id: 10,
-    img: 'img/char-10.png',
-    matched: false,
-    flipped: false
-},
-{
-    id: 10,
-    img: 'img/char-10.png',
+    match_id: 5,
+    img: 'img/char-5.png',
     matched: false,
     flipped: false
 },
 {
     id: 11,
-    img: 'img/char-11.png',
-    matched: false,
-    flipped: false
-},
-{
-    id: 11,
-    img: 'img/char-11.png',
+    match_id: 6,
+    img: 'img/char-6.png',
     matched: false,
     flipped: false
 },
 {
     id: 12,
+    match_id: 6,
+    img: 'img/char-6.png',
+    matched: false,
+    flipped: false
+},
+{
+    id: 13,
+    match_id: 7,
+    img: 'img/char-7.png',
+    matched: false,
+    flipped: false
+},
+{
+    id: 14,
+    match_id: 7,
+    img: 'img/char-7.png',
+    matched: false,
+    flipped: false
+},
+{
+    id: 15,
+    match_id: 8,
+    img: 'img/char-8.png',
+    matched: false,
+    flipped: false
+},
+{
+    id: 16,
+    match_id: 8,
+    img: 'img/char-8.png',
+    matched: false,
+    flipped: false
+},
+{
+    id: 17,
+    match_id: 9,
+    img: 'img/char-9.png',
+    matched: false,
+    flipped: false
+},
+{
+    id: 18,
+    match_id: 9,
+    img: 'img/char-9.png',
+    matched: false,
+    flipped: false
+},
+{
+    id: 19,
+    match_id: 10,
+    img: 'img/char-10.png',
+    matched: false,
+    flipped: false
+},
+{
+    id: 20,
+    match_id: 10,
+    img: 'img/char-10.png',
+    matched: false,
+    flipped: false
+},
+{
+    id: 21,
+    match_id: 11,
+    img: 'img/char-11.png',
+    matched: false,
+    flipped: false
+},
+{
+    id: 22,
+    match_id: 11,
+    img: 'img/char-11.png',
+    matched: false,
+    flipped: false
+},
+{
+    id: 23,
+    match_id: 12,
     img: 'img/char-12.png',
     matched: false,
     flipped: false
 },
 {
-    id: 12,
+    id: 24,
+    match_id: 12,
     img: 'img/char-12.png',
     matched: false,
     flipped: false
@@ -151,74 +175,80 @@ let point = 0;
 let clicked = 0
 let is_clicked = []
  
-// src.appendChild(img); 
+randomize_array(data)
+document.querySelector('#score').innerHTML = `${point}`;
 
 
-
-
-function render(){
-
-    // var src = document.getElementById("score"); 
-    // var text = document.createTextNode(point); 
-
-    // src.appendChild(text); 
-
-    // var src = document.querySelector("#score").append(point);
-     
-    // // var text = document.createTextNode(point); 
-    // console.log(src)
-    // console.log(point)
-    // // src.appendChild(text);
-
-    
-
-    for(let i=0; i<data.length; i++ ){
-        
-        
-        let brick = data[i];
-
-        let el = document.createElement('article');
-        el.innerHTML = `<img src="${brick.img}">`; 
-        el.addEventListener("click", () =>{
-            // while(clicked<3){
-            el.classList.toggle('clicked')
-
-            is_clicked.push(brick);
-            is_clicked.at(-1).flipped = true
-            console.log(is_clicked[0].flipped)
-
-            // }
-            if (is_clicked.length == 2){
-                check_if_same()
-                // is_clicked.at(-1).flipped = false
-                // is_clicked = is_clicked.pop()
-                turn_card()
-
-            }
-
-        });
-
-        document.querySelector('#x').append(el);
-
-    }
-
+function randomize_array(array){
+    for(let i=array.length-1; i > 0; i--){
+        const j = Math.floor(Math.random() * i)
+        const temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+      }
 }
 
-function turn_card(){
-    let test = document.querySelector('#score') 
-    test.innerHTML = `<p>${point}</p>`; 
-    document.querySelector(".clicked").classList.toggle('clicked')
-    document.querySelector(".clicked").classList.toggle('clicked')
 
+function render(){  
+
+    for(let i=0; i<data.length; i++ ){
+        let brick = data[i];
+        let el = document.createElement('article');
+        el.innerHTML = `<img id= brick_id_${brick.id} src="${brick.img}">`;
+        el.addEventListener("click", () =>{
+            if (brick.matched != true){
+            is_clicked.push(brick);
+            is_clicked.at(-1).flipped = true
+            turn_card()
+            time_to_check()
+            
+            }
+        });
+        document.querySelector('#x').append(el);
+    }
+}
+
+
+
+function turn_card(){
+    for(let i=0; i<data.length; i++ ){
+        let brick = data[i];
+        if(brick.flipped === true){
+            document.querySelector(`#brick_id_${brick.id}`).classList.add('clicked')
+        } else{
+            document.querySelector(`#brick_id_${brick.id}`).classList.remove('clicked')
+        }
+    }
+}
+
+function time_to_check(){
+    if (is_clicked.length == 2){
+        if(is_clicked[0].id === is_clicked[1].id){
+            is_clicked.pop()
+        }else{
+            check_if_same()
+        }
+
+    }
 }
 
 function check_if_same(){
-    if (is_clicked[0].id === is_clicked[1].id){
-        console.log("wow")
-        console.log(point)
+    if (is_clicked[0].match_id === is_clicked[1].match_id){
+        
+        is_clicked[0].matched = true
+        is_clicked[1].matched = true
+    } else{
+        is_clicked[0].flipped = false
+        is_clicked[1].flipped = false
+
         point ++
+        document.querySelector('#score').innerHTML = `${point}`;
+
+
     }
     is_clicked = []
+
+
 
 }
 
